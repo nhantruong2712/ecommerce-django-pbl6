4053cf14f4433541a1032684416ca8e2bc3a3045
nhantruong2712 <trconghiennhan2712@gmail.com>
2021-11-20 01:21:11 +0700

    update

--- djecommerce/wsgi.py
+++ djecommerce/wsgi.py
@@ -3,7 +3,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djecommerce.settings.base')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djecommerce.settings')
 
 application = get_wsgi_application()
 
