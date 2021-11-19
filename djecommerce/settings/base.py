f68514b94a6bc6433990cb66e0b8efb679b99345
nhantruong2712 <trconghiennhan2712@gmail.com>
2021-11-19 22:01:30 +0700

    update setting

--- djecommerce/settings/base.py
+++ djecommerce/settings/base.py
@@ -1,5 +1,6 @@
 import os
 from decouple import config
+import dj_database_url
 
 BASE_DIR = os.path.dirname(os.path.dirname(
     os.path.dirname(os.path.abspath(__file__))))
@@ -76,7 +77,12 @@ USE_L10N = True
 USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
-
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'db.sqlite3',
+    }
+}
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static_in_env')]
