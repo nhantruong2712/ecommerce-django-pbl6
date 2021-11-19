bafd10a8e639a852cf35765061a9b7ba54c15958
nhantruong2712 <trconghiennhan2712@gmail.com>
2021-11-20 00:34:38 +0700

    collect static

--- djecommerce/settings/base.py
+++ djecommerce/settings/base.py
@@ -85,9 +85,9 @@ DATABASES = {
 }
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static_in_env')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media_root')
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static_in_env')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Auth
 
